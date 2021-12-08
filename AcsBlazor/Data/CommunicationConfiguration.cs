714d0d9d56afb1cd771f6762a4108ada4d7cbbff
Petr Švihlík <petrsvihlik@microsoft.com>
2021-12-08 11:51:29 +0100

    Add project files.

---
+++ AcsBlazor/Data/CommunicationConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace AcsBlazor.Data
+{
+    public class CommunicationConfiguration
+    {
+        public Uri Endpoint { get; set; }
+
+        public string AccessKey { get; set; }
+    }
+}
